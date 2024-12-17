06ad83ff7743c3abb1e08c3ddb20b05d0e575961
Coding1610 <yp.70010@gmail.com>
2024-12-17 09:39:13 +0530

    React Redux

---
+++ React_Redux_Tutorial_3/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+    <div className='border-x-8 border-y-8 flex flex-col justify-center'>
+        <div className='w-full flex flex-col'>
+            <p className=''> Welcome, to React Redux Toolkit </p>
+            <p className=''> ADMIN TABLE </p>
+            <p className=''> One Destination for complete Web Development</p>
+        </div>
+        <div></div>
+    </div>
+    </>
+  )
+}  
